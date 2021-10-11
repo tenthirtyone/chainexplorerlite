41b5cd5e760f996e589b062dfc5584203ef382a7
tenthirtyone <alexsherbuck@gmail.com>
2021-10-11 12:47:13 -0500

    move highestBlock from infura to cache

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const API = require("./lib/api");
-const DB = require("./lib/db");
+
 const Infura = require("./lib/infura");
 const Reporter = require("./lib/reporter");
 const { sharedCache } = require("./lib/cache");
@@ -13,37 +13,17 @@ class Explorer {
     this.api = new API();
     this.api.express.set("explorer", this);
     this.infura = new Infura();
-    this.db = new DB();
+
     this.reporter = new Reporter();
     this.sharedCache = sharedCache;
   }
 
   start() {
     this.logger.info("App starting...");
-    this.populateCache();
+
     this.api.start();
     this.infura.start();
     this.logger.info("App started.");
-  }
-
-  async populateCache() {
-    const { cacheLimit } = this.config;
-    let blocks = [];
-
-    try {
-      blocks = await this.db.Block.findAll({
-        include: this.db.Transaction,
-        limit: cacheLimit,
-      });
-      blocks.forEach((block) => {
-        this.sharedCache.add(block.number, block);
-      });
-      this.infura.highestBlock = blocks[blocks.length - 1].number;
-    } catch (e) {
-      this.logger.error(
-        "Error Populating Cache - if this is the first run/no blocks table it is safe to ignore this"
-      );
-    }
   }
 
   async createReport(startBlock, endBlock) {
@@ -65,9 +45,7 @@ class Explorer {
   }
 
   static get DEFAULTS() {
-    return {
-      cacheLimit: 1e10,
-    };
+    return {};
   }
 }
 
