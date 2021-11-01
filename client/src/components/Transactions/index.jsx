e9f0e97ff5c5782f84cf75bf5c5d0cd73579fbc7
tenthirtyone <alexsherbuck@gmail.com>
2021-11-01 03:23:39 -0500

    filter by address for sender/receiver

--- client/src/components/Transactions/index.jsx
+++ client/src/components/Transactions/index.jsx
@@ -1,42 +1,73 @@
 import React, { Fragment, useState } from "react";
 import Grid from "@material-ui/core/Grid";
-import Card from "../Card";
+import Card from "@material-ui/core/Card";
+import SummaryCard from "../SummaryCard";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import AddressFilter from "./AddressFilter";
 
 function TransactionList({ txs, contractDictionary, heading }) {
   const defaultPageSize = 25;
   const [pageSize, setPageSize] = useState(defaultPageSize);
+  const [filterAddresses, setFilterAddresses] = useState({});
 
   function loadMore() {
     setPageSize(pageSize + defaultPageSize);
   }
 
-  const data = Object.keys(txs);
+  let data = Object.keys(txs);
+
+  if (data.length === 0) return null;
+
+  let filter = [];
+
+  for (const [key, data] of Object.entries(filterAddresses)) {
+    if (data.tracked) {
+      filter.push(data.address);
+    }
+  }
+
+  if (filter.length > 0) {
+    data = data.filter((address) => {
+      return filter.indexOf(address) >= 0;
+    });
+  }
 
   return (
     <Fragment>
-      {data.length > 0 && <Typography variant="h4">{heading}</Typography>}
-      {data.slice(0, pageSize).map((key) => {
-        return (
-          <Card
-            key={key}
-            label={key}
-            heading={txs[key]}
-            isContract={contractDictionary.indexOf(key)}
-          />
-        );
-      })}
-      {pageSize < data.length && (
-        <Button variant="contained" onClick={loadMore}>
-          Load More
-        </Button>
-      )}
+      <Card variant="outlined">
+        <Typography variant="h4">{heading}</Typography>
+
+        <AddressFilter
+          heading={heading}
+          filterAddresses={filterAddresses}
+          setFilterAddresses={setFilterAddresses}
+        />
+
+        {data.slice(0, pageSize).map((key, index) => {
+          return (
+            <SummaryCard
+              key={key}
+              label={key}
+              heading={txs[key]}
+              isContract={contractDictionary.indexOf(key)}
+              style={{
+                backgroundColor: index % 2 === 0 ? "#ffffff" : "#dfdfdf",
+              }}
+            />
+          );
+        })}
+        {pageSize < data.length && (
+          <Button variant="contained" onClick={loadMore}>
+            Load More
+          </Button>
+        )}
+      </Card>
     </Fragment>
   );
 }
 
-export default function Transaction({ senders, receivers, contracts }) {
+export default function Transactions({ senders, receivers, contracts }) {
   return (
     <Grid container spacing={4}>
       <Grid item xs={12} md={6}>
